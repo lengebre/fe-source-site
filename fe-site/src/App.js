f0f3186b7d8aae1276cde31056ec4335fbb77a77
lengebre <>
2021-12-26 14:34:31 -0500

    (main)Add application to repo

---
+++ fe-site/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App__Header">
+        <h1> This Site Works!!!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
