adf1389ccf3c18a6577a7c0a6b8772d2d61a822e
lengebre <>
2021-12-26 20:01:16 -0500

    (main) Update site and add Development Dependencies

--- fe-site/src/App.js
+++ fe-site/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App__Header">
-        <h1> This Site Works!!!</h1>
+        <h1> FE Site</h1>
       </header>
     </div>
   );
